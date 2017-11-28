1b92dd71a70a86cf4bb9e00d77afd9558b61f6de
Benj972 <ben.gallot972@gmail.com>
2017-11-28 11:10:02 +0100

    create form addAction in file TrickType

---
+++ src/SnowTricks/HomeBundle/Form/TrickType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SnowTricks\HomeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+/*use Symfony\Component\Form\Extension\Core\Type\CollectionType;*/
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('name', TextType::class)
+        ->add('content', TextareaType::class)
+        /*->add('images', CollectionType::class, array(
+        'entry_type'   => ImageType::class,
+        'allow_add'    => true,
+        'allow_delete' => true
+      ))*/
+        ->add('save',  SubmitType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SnowTricks\HomeBundle\Entity\Trick'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'snowtricks_homebundle_trick';
+    }
+
+
+}
