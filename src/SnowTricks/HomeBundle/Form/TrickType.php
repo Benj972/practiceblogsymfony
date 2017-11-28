ea377104293cab736b291f6613adba3e49f5870f
Benj972 <ben.gallot972@gmail.com>
2017-11-28 19:03:49 +0100

    integrate nested forms Category and Image

--- src/SnowTricks/HomeBundle/Form/TrickType.php
+++ src/SnowTricks/HomeBundle/Form/TrickType.php
@@ -6,12 +6,15 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-/*use Symfony\Component\Form\Extension\Core\Type\CollectionType;*/
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TrickType extends AbstractType
 {
+
+
     /**
      * {@inheritdoc}
      */
@@ -20,11 +23,16 @@ class TrickType extends AbstractType
         $builder
         ->add('name', TextType::class)
         ->add('content', TextareaType::class)
-        /*->add('images', CollectionType::class, array(
+        ->add('category', EntityType::class, array(
+        'class'         => 'SnowTricksHomeBundle:Category',
+        'choice_label'  => 'name',
+        'multiple'      => true,
+        ))
+        ->add('images', CollectionType::class, array(
         'entry_type'   => ImageType::class,
         'allow_add'    => true,
         'allow_delete' => true
-      ))*/
+         ))
         ->add('save',  SubmitType::class);
     }
     
