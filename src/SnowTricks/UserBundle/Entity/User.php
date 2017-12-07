eb74da0916cdac966828def5b525b905aa46c42f
Benj972 <ben.gallot972@gmail.com>
2017-12-07 17:12:23 +0100

    Create entity User heritage FOSUserBundle

---
+++ src/SnowTricks/UserBundle/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SnowTricks\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="SnowTricks\UserBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
+
