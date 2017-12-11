cdfdf03d540c0aa441fcd54ce303d7b1941beae9
Benj972 <ben.gallot972@gmail.com>
2017-12-11 19:37:35 +0100

    create service security

---
+++ src/SnowTricks/HomeBundle/Security/LoginFormAuthenticator.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SnowTricks\HomeBundle\Security;
+
+use SnowTricks\HomeBundle\Entity\User;
+use SnowTricks\HomeBundle\Form\LoginType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+	private $formFactory;
+	private $em;
+	private $router;
+
+	public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router)
+    {
+    	$this->formFactory = $formFactory;
+    	$this->em = $em;
+    	$this->router = $router;
+    }
+
+	public function getCredentials(Request $request)
+    {
+    	$isLoginSubmit = $request->getPathInfo() == '/login' && $request->isMethod('POST');
+    	if (!$isLoginSubmit) {
+            // skip authentication
+            return;
+        }
+
+        $form = $this->formFactory->create(LoginType::class);
+        $form->handleRequest($request);
+
+        $data = $form->getData();
+
+        return $data;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+    	$username = $credentials['_username'];
+
+    	return $this->em->getRepository('SnowTricksHomeBundle:User')
+            ->findOneBy(['email' => $username]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+    	$password = $credentials['_password'];
+
+    	if ($password == 'iliketurtles') {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function getLoginUrl()
+    {
+    	return $this->router->generate('snow_tricks_home_login');
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // on success, let the request continue
+        return null;
+    }
+
+    protected function getDefaultSuccessRedirectUrl()
+    {
+    }
+}
