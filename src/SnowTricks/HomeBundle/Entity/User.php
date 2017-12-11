a1100f906ce9720a74d053cdf0c0312f7ec3441d
Benj972 <ben.gallot972@gmail.com>
2017-12-11 15:44:21 +0100

    add entity user

---
+++ src/SnowTricks/HomeBundle/Entity/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SnowTricks\HomeBundle\Entity;
+
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+
+class User implements UserInterface
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+
+    }
+
+    public function getSalt()
+    {
+
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function eraseCredentials()
+    {
+
+    }
+}
