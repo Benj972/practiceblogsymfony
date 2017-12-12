9b5a55de54c92781ce055cee9890f002120fd113
Benj972 <ben.gallot972@gmail.com>
2017-12-12 19:11:35 +0100

    implement passwordEncoder in loginform

--- src/SnowTricks/HomeBundle/Security/LoginFormAuthenticator.php
+++ src/SnowTricks/HomeBundle/Security/LoginFormAuthenticator.php
@@ -14,18 +14,21 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
 	private $formFactory;
 	private $em;
 	private $router;
+    private $passwordEncoder;
 
-	public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router)
+	public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router, UserPasswordEncoder $passwordEncoder)
     {
     	$this->formFactory = $formFactory;
     	$this->em = $em;
     	$this->router = $router;
+        $this->passwordEncoder = $passwordEncoder;
     }
 
 	public function getCredentials(Request $request)
@@ -59,10 +62,10 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
     {
     	$password = $credentials['_password'];
 
-    	if ($password == 'iliketurtles') {
+    	if ($this->passwordEncoder->isPasswordValid($user, $password)) {
             return true;
         }
-
+        
         return false;
     }
 
