35074e66f9a0da4d8da8e5a8c57d5754be35abd8
Benj972 <ben.gallot972@gmail.com>
2017-12-19 13:21:17 +0100

    create form resetpassword

---
+++ src/SnowTricks/HomeBundle/Form/ResetPasswordType.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace SnowTricks\HomeBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\HttpFoundation\Request;
+use SnowTricks\HomeBundle\Form\Model\ResetPassword;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use SnowTricks\HomeBundle\Manager\UserManagerInterface;
+
+class ResetPasswordType extends AbstractType
+{
+    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('password', RepeatedType::class, array(
+            'first_options'  => array('label' => PasswordType::class),
+            'second_options' => array('label' => RepeatedType::class),
+            'type'           => PasswordType::class,
+        ));
+        $builder->add('Reset Password', 'submit');
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'SnowTricks\HomeBundle\Form\Model\ResetPassword',
+        ]);
+    }
+} 
+
