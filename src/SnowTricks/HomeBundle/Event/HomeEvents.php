941005fe60f03840599b6b1dffa0693794e83440
Benj972 <ben.gallot972@gmail.com>
2017-12-19 12:56:17 +0100

    create folder Event

---
+++ src/SnowTricks/HomeBundle/Event/HomeEvents.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SnowTricks\HomeBundle\Event;
+
+final class HomeEvents
+{
+    const NEW_ACCOUNT_CREATED = 'snow_tricks_homebundle.new_account_created';
+    const NEW_PASSWORD_REQUESTED = 'snow_tricks_homebundle.new_password_requested';
+} 
