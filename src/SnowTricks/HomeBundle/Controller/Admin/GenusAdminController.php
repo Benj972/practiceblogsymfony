5532411c0df009598d86163b67cbda5bd128ed26
Benj972 <ben.gallot972@gmail.com>
2017-12-19 13:33:57 +0100

    uptdate file

--- src/SnowTricks/HomeBundle/Controller/Admin/GenusAdminController.php
+++ src/SnowTricks/HomeBundle/Controller/Admin/GenusAdminController.php
@@ -2,23 +2,21 @@
 
 namespace SnowTricks\HomeBundle\Controller\Admin;
 
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Security("is_granted('ROLE_USER')")
+ * @Security("is_granted('ROLE_ADMIN')")
  */
 class GenusAdminController extends Controller
 {
-	/**
-     * @Security("is_granted('ROLE_USER')")
-     */
+	
 	public function indexAction()
     {
-    	$this->denyAccessUnlessGranted('ROLE_USER');
-        if (!$this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+    	$this->denyAccessUnlessGranted('ROLE_ADMIN');
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
             throw $this->createAccessDeniedException('GET OUT!');
         }
 
