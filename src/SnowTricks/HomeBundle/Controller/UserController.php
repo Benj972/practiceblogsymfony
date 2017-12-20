bb2364b73a0cc42a28ce3acc1882e278fa166e21
Benj972 <ben.gallot972@gmail.com>
2017-12-20 11:56:34 +0100

    implement resetPasswordAction and correction

--- src/SnowTricks/HomeBundle/Controller/UserController.php
+++ src/SnowTricks/HomeBundle/Controller/UserController.php
@@ -2,20 +2,21 @@
 
 namespace SnowTricks\HomeBundle\Controller;
 
+use SnowTricks\HomeBundle\Entity\User;
 use SnowTricks\HomeBundle\Form\UserRegistrationType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use SnowTricks\HomeBundle\Form\ChangePasswordType;
-use SnowTricks\HomeBundle\Form\Model\ChangePassword;
-use SnowTricks\HomeBundle\Entity\User;
-use SnowTricks\HomeBundle\Form\Model\ResetPassword;
-use SnowTricks\HomeBundle\Form\Model\RequestPassword;
 use SnowTricks\HomeBundle\Form\ResetPasswordType;
 use SnowTricks\HomeBundle\Form\RequestPasswordType;
+use SnowTricks\HomeBundle\Form\Model\ChangePassword;
+use SnowTricks\HomeBundle\Form\Model\ResetPassword;
+use SnowTricks\HomeBundle\Form\Model\RequestPassword;
+use SnowTricks\HomeBundle\Event\HomeEvents;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
-use SnowTricks\HomeBundle\Event\HomeEvents;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class UserController extends Controller
 {
@@ -30,7 +31,7 @@ class UserController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
-            $this->addFlash('success', 'Welcome '.$user->getEmail());
+            $this->addFlash('info', 'Welcome '.$user->getEmail());
             return $this->redirectToRoute('snow_tricks_home_homepage');
         }
         return $this->render('SnowTricksHomeBundle:User:register.html.twig', [
@@ -43,26 +44,25 @@ class UserController extends Controller
         $changePasswordModel = new ChangePassword();
         $oldPassword = $changePasswordModel->getOldPassword();
         $newPassword = $changePasswordModel->getNewPassword();
-        $user = $this->getUser();
-        $encoder = $this->container->get('security.password_encoder');
-        $oldplainPassword = $encoder->encodePassword($user, $oldPassword);
-      
+        
         $form = $this->createForm(ChangePasswordType::class, $changePasswordModel);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-        
+        $user = $this->getUser();
+        $encoder = $this->container->get('security.password_encoder');
+        $oldplainPassword = $encoder->encodePassword($changePasswordModel, $oldPassword);
+
           if ($user->getPassword() != $oldplainPassword){
-            $request->getSession()->getFlashBag()->add('info', "Wrong old password!");
+            $this->addFlash('info', "Wrong old password!");
           } else {
             $newplainPassword = $encoder->encodePassword($user, $newPassword);
             $user->setPassword($newplainPassword);
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
-            $request->getSession()->getFlashBag()->add('info', "Password change successfully!");
+            $this->addFlash('info', "Password change successfully!");
           }
-          // perform some action,
-          // such as encoding with MessageDigestPasswordEncoder and persist
+
           return $this->redirectToRoute('snow_tricks_home_homepage');
       }
 
@@ -74,12 +74,24 @@ class UserController extends Controller
     public function resetPasswordAction(Request $request)
     {
             $resetpassword = new ResetPassword;
+            $newpassword = $resetpassword->getPassword();
 
             $form = $this->createForm(ResetPasswordType::class, $resetpassword);
             $form->handleRequest($request);
             if ($form->isSubmitted() && $form->isValid()) {
-            $request->getSession()->getFlashBag()->add('info', "Your password has been resetted. You can login now.");  
-            return $this->redirectToRoute('snow_tricks_home_homepage');
+                $passwordEncoder = $this->container->get('security.password_encoder');
+                $password = $passwordEncoder->encodePassword($resetpassword, $newpassword);
+                $user->setPassword($password);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+           
+                $securityToken = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+                $this->get('security.token_storage')->setToken($securityToken);
+
+                $this->addFlash('info', "Your password has been resetted. You can login now.");  
+                return $this->redirectToRoute('snow_tricks_home_homepage');
             }
 
             return $this->render('SnowTricksHomeBundle:User:resetPassword.html.twig', array(
@@ -90,19 +102,20 @@ class UserController extends Controller
     public function requestPasswordAction(Request $request)
     {
         $requestpassword = new RequestPassword;
+        $identifier = $requestpassword->getIdentifier();
 
         $form = $this->createForm(RequestPasswordType::class, $requestpassword);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            if{ 
-                $user = $this->get('security.token_storage')->getToken()->getUser();
+        if ($form->isSubmitted() && $form->isValid()) { 
+        		/*$user = $this->getUser();
+            	$user->getEmail() = $identifier;*/ 
                 $event = new RequestPassword();
+                /*$user = $this->get('security.token_storage')->getToken()->getUser();*/
                 // On déclenche l'évènement
-                $this->get('event_dispatcher')->dispatch(HomeEvents::NEW_ACCOUNT_CREATED, $event);
-                $request->getSession()->getFlashBag()->add('info', "A mail has been sent to your mailbox to reset your password.");  
+                $this->get('event_dispatcher')->dispatch(HomeEvents::NEW_PASSWORD_REQUESTED, $event);
+                
+                $this->addFlash('info', "A mail has been sent to your mailbox to reset your password.");  
                 return $this->redirectToRoute('snow_tricks_home_homepage');
-            }
         }
 
         return $this->render('SnowTricksHomeBundle:User:requestPassword.html.twig', [
