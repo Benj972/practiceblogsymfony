51a0c628973769456e4dda9138ca432c6109b5bb
Benj972 <ben.gallot972@gmail.com>
2017-12-20 11:43:53 +0100

    correction error

--- src/SnowTricks/HomeBundle/Event/HomeEvents.php
+++ src/SnowTricks/HomeBundle/Event/HomeEvents.php
@@ -6,6 +6,5 @@ use Symfony\Component\EventDispatcher\Event;
 
 final class HomeEvents extends Event
 {
-    const NEW_ACCOUNT_CREATED = 'snow_tricks_homebundle.new_account_created';
     const NEW_PASSWORD_REQUESTED = 'snow_tricks_homebundle.new_password_requested';
 } 
