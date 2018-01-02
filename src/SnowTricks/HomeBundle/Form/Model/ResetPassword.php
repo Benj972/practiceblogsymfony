9405dc89ea938db57bb11ce319a4d1f4c99f9002
Benj972 <ben.gallot972@gmail.com>
2018-01-02 10:55:47 +0100

    correction ResetPassword model

--- src/SnowTricks/HomeBundle/Form/Model/ResetPassword.php
+++ src/SnowTricks/HomeBundle/Form/Model/ResetPassword.php
@@ -4,44 +4,29 @@ namespace SnowTricks\HomeBundle\Form\Model;
 
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
-use SnowTricks\HomeBundle\Entity\User;
 
-    class ResetPassword 
+class ResetPassword 
 {
     /**
      * @Assert\NotBlank()
      * @Assert\Length(min=8)
      */
-    private $password;
+    private $plainPassword;
     /**
      * @param string $password
      */
 
-    public function setPassword($password)
+    public function setPlainPassword($plainPassword)
     {
-        $this->password = $password;
+        $this->plainPassword = $plainPassword;
     }
 
     /**
      * @return string
      */
-    public function getPassword()
+    public function getPlainPassword()
     {
-        return $this->password;
+        return $this->plainPassword;
     }
-
-    /*abstract public function getRoles();
-
-    abstract public function getSalt();
-
-    abstract public function getUsername();
-
-    abstract public function getEmail();
-
-    abstract public function getAvatar();
-
-    abstract public function getToken();
-
-    abstract public function getPlainPassword();*/
 
 }
