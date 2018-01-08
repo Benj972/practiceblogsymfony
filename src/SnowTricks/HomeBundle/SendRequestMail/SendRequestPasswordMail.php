f9680c2fa73f29c04af679ef16c93498fde5e086
Benj972 <ben.gallot972@gmail.com>
2018-01-08 16:03:22 +0100

    implement component templating

--- src/SnowTricks/HomeBundle/SendRequestMail/SendRequestPasswordMail.php
+++ src/SnowTricks/HomeBundle/SendRequestMail/SendRequestPasswordMail.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\RouterInterface;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use SnowTricks\HomeBundle\Entity\User;
+use Symfony\Component\Templating\EngineInterface;
 
 class SendRequestPasswordMail
 {
@@ -16,12 +17,14 @@ class SendRequestPasswordMail
     protected $mailer;
   
     protected $router;
-   
+       
 
-    public function __construct(\Swift_Mailer $mailer, RouterInterface $router)
+    public function __construct(\Swift_Mailer $mailer, RouterInterface $router, EngineInterface $templating)
     {
         $this->mailer = $mailer;
         $this->router = $router;
+        $this->templating = $templating;
+             
     }
     /**
      * @param UserDataEvent $event
@@ -33,7 +36,14 @@ class SendRequestPasswordMail
             ->setSubject('SnowTricks : Récupération de votre mot de passe')
             ->setFrom('ben.gallot972@gmail.com')
             ->setTo($user->getEmail())
-            ->setBody('<a href="'. $this->router->generate('snow_tricks_home_reset_password', ['token' => $user->getToken()], UrlGeneratorInterface::ABSOLUTE_URL).'">Cliquez ici pour réinitialiser votre mot de passe</a>,"text/html"');
+            ->setBody($this->templating->render('SnowTricksHomeBundle:User:request_password_mail.html.twig',
+                [
+                'username' => $user->getUsername(),
+                'request_link' => $this->router->generate('snow_tricks_home_reset_password',
+                    ['token' => $user->getToken()], UrlGeneratorInterface::ABSOLUTE_URL)
+            ])
+        );
+            /*->setBody('<a href="'. $this->router->generate('snow_tricks_home_reset_password', ['token' => $user->getToken()], UrlGeneratorInterface::ABSOLUTE_URL).'">Cliquez ici pour réinitialiser votre mot de passe</a>,"text/html"');*/
 
         $this->mailer->send($message);
     }
