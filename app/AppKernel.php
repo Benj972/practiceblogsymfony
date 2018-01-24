5139911ea6148999a0681c1d4cbf491f9bcb8a2c
Benj972 <ben.gallot972@gmail.com>
2018-01-24 16:26:29 +0100

    delete CoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new SnowTricks\HomeBundle\SnowTricksHomeBundle(),
-            new SnowTricks\CoreBundle\SnowTricksCoreBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         ];
